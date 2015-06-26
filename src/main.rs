d9130bb2f61232a167226e2e5efa12cd98e9532c
Britt Cagnina <brittcagnina@gmail.com>
2015-06-25 20:00:18 -0500

    Abstracted to Tensor, added print() and get()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,73 @@
 pub mod tensor;
-pub use tensor::{Rank1Tensor, Rank2Tensor};
+pub use tensor::{Rank1Tensor, Rank2Tensor, Tensor};
 
 fn main() {
-    let x1 = vec![1,0];
-    let x2 = vec![0,1];
+    //let x1 = vec![1,0];
+    //let x2 = vec![0,1];
 
-    let v1 = Rank1Tensor::build(2, x1);
-    let v2 = Rank1Tensor::build(2, x2); 
+    //let v1 = Rank1Tensor::build(2, x1);
+    //let v2 = Rank1Tensor::build(2, x2); 
 
-    let t1 = v1 * v2;
-    t1.print();
+    //let t1 = v1 * v2;
+
+    //let a1 = vec![1,0];
+    //let a2 = vec![0,1];
+    
+    //3-d Rank2 Tensor
+    let t2 = Tensor::build(3, 2, vec![
+        1, 2, 3, 
+        4, 5, 6, 
+        7, 8, 9
+    ]);
+    
+    //4-d Rank1 Tensor
+    let t1 = Tensor::build(4, 1, vec![
+        2, 3, 5, 5
+    ]); 
+
+    //3d Rank3 Tensor
+    let t3 = Tensor::build(3, 3, vec![
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9, 
+       
+        10, 11, 12,
+        13, 14, 15,
+        16, 17, 18,
+
+        19, 20, 21,
+        22, 23, 24,
+        25, 26, 27 
+    ]);
+       
+
+    //2-d Rank2 Tensor
+    let t4 = Tensor::build(2, 2, vec![
+        1, 2, 
+        3, 4
+    ]);
+
+    //2-d Rank3 Tensor
+    let t5 = Tensor::build(2, 3, vec![
+        1, 2,
+        3, 4,
+ 
+        5, 6,
+        7, 8
+    ]); 
+
+    println!( "Printing t1" );
+    t1.print(); 
+
+    println!( "Printing t2" );
+    t2.print();
+
+    println!( "Printing t3" );
+    t3.print();
+
+    println!( "Printing t4" );
+    t4.print();
+
+    println!( "Printing t5" );
+    t5.print();
 }
--- src/tensor.rs
+++ src/tensor.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Mul};
+use std::ops::Mul;
 
 pub struct Rank1Tensor {
     dim: i32,
@@ -7,7 +7,81 @@ pub struct Rank1Tensor {
 
 pub struct Rank2Tensor {
     dim: i32,
-    components: Vec<Vec<i64>>
+    components: Vec<Vec<i64>>,
+}
+
+pub struct Rank3Tensor {
+    dim: i32,
+    components: Vec<Vec<Vec<i64>>>,
+}
+
+//generalized tensor
+pub struct Tensor {
+   dim: i32,
+   rank: i32,
+   components: Vec<i64>, 
+}
+
+//generalized tensor impl
+impl Tensor {
+    pub fn build(d: i32, r: i32, v: Vec<i64>) -> Tensor {
+        Tensor {
+            //dim of basic vector
+            dim: d,
+            rank: r,
+            components: v,
+        } 
+    } 
+    //O(rank) 
+    pub fn get(&self, indices: &[i32]) -> i64 {
+        let mut i: i32 = 0;
+        //We can build dim^(rank) ahead of time
+        for x in 0..self.rank {
+            i = i + scalar_power(self.dim, x as i32) * indices[x as usize];
+        }     
+        self.components[i as usize].clone() 
+    }
+    fn print_element(&self, v: Vec<i32>) {
+        println!( "T{:?} : {:?}", v, self.get(&v) ); 
+    }
+    pub fn print(&self) {
+       let mut v: Vec<i32> = Vec::new();
+       for _ in 0..self.rank {
+           v.push(self.dim.clone());
+       }
+       vloop(v, Tensor::print_element, &self);
+    }
+}
+
+//temp solution for n^p 
+pub fn scalar_power(n: i32, p: i32) -> i32 {
+    let mut a: i32 = 1;
+    for _ in 0..p {
+        a = a*n;
+    }
+    a
+}
+
+//entry point for vloop_many
+pub fn vloop(max_indices: Vec<i32>, f: fn(&Tensor, Vec<i32>), t: &Tensor) {
+    vloop_many(max_indices.clone(), f, t, Vec::new(), 0);
+}
+
+//variable depth loop
+pub fn vloop_many(max_indices: Vec<i32>, f: fn(&Tensor, Vec<i32>), t: &Tensor, pargs: Vec<i32>, index: i32) {
+    if max_indices.len() == 0 {
+        f(t, pargs); 
+    } else {
+        let mut args = pargs.clone();
+        let rest: Vec<i32> = max_indices[1..].to_vec();
+        for _ in 0..max_indices[0] {
+            if args.len() == index as usize { args.push(0); }
+            if args[index as usize] < max_indices[0] {
+                vloop_many(rest.clone(), f, t, args.clone(), index + 1);
+                args[index as usize] = args[index as usize] + 1;
+            }
+        }
+    }
 }
 
 impl Rank1Tensor {
@@ -23,8 +97,8 @@ impl Rank1Tensor {
             components: c,
         }
     }
-    pub fn get(&self, i: usize) -> i64 {
-       self.components[i].clone() 
+    pub fn get(&self, i: i32) -> i64 {
+       self.components[i as usize].clone() 
     }
     pub fn dim(&self) -> i32 {
         self.dim.clone()
@@ -34,7 +108,7 @@ impl Rank1Tensor {
 impl Rank2Tensor {
     pub fn new(d: i32) -> Rank2Tensor { 
         let mut temp_vec = Vec::new();
-        for i in 0..d {
+        for _ in 0..d {
             temp_vec.push(Vec::new());
         }
         Rank2Tensor { 
@@ -48,8 +122,8 @@ impl Rank2Tensor {
             components: c,
         }
     }
-    pub fn get(&self, i: usize, j: usize) -> i64 {
-        self.components[i][j].clone()
+    pub fn get(&self, i: i32, j: i32) -> i64 {
+        self.components[i as usize][j as usize].clone()
     } 
     pub fn dim(&self) -> i32 {
         self.dim.clone()
@@ -57,9 +131,38 @@ impl Rank2Tensor {
     pub fn print(&self) {
         for i in 0..self.dim() {
             for j in 0..self.dim() {
-                print!{ "T{}{}: {}\n", i, j, self.get(i as usize, j as usize) };
+                print!{ "T[{},{}]: {}\n", i, j, self.get(i, j) };
             }
         }
+    }
+}
+
+impl Rank3Tensor {
+    pub fn new(d: i32) -> Rank3Tensor { 
+        let mut temp_vec = Vec::new();
+        for _ in 0..d {
+            let mut temp_vec_2 = Vec::new();
+            for _ in 0..d {
+                temp_vec_2.push(Vec::new());
+            }
+            temp_vec.push(temp_vec_2);
+        }
+        Rank3Tensor { 
+            dim: d, 
+            components: temp_vec,
+        } 
+    }
+    pub fn build(d: i32, c: Vec<Vec<Vec<i64>>>) -> Rank3Tensor {
+        Rank3Tensor {
+            dim: d,
+            components: c,
+        }
+    }
+    pub fn get(&self, i: i32, j: i32, k: i32) -> i64 {
+       self.components[i as usize][j as usize][k as usize].clone() 
+    }
+    pub fn dim(&self) -> i32 {
+        self.dim.clone()
     }
 }
 
@@ -70,7 +173,7 @@ impl Mul<Rank1Tensor> for Rank1Tensor {
         for i in 0..self.dim() {
             let mut temp_vec = Vec::new();
             for j in 0..other.dim() {
-                temp_vec.push(self.get(i as usize) * other.get(j as usize))
+                temp_vec.push(self.get(i) * other.get(j))
             }
             vec.push(temp_vec)
         }
